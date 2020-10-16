81e75ffa85b3faeff47eb5e738c3e68563688de3
Caleb Short <calebshort4697@gmail.com>
2020-10-15 21:04:30 -0400

    Got pacman to move

--- main.py
+++ main.py
@@ -14,37 +14,28 @@ gui.configure(background="black")
 gui.geometry("700x700")
 center_tk_window.center_on_screen(gui)
 
-def initialize_objects():
-    object_pacman = dict()
-    object_pacman["coordinates"] = (0,0)
-    object_pacman["velocity"] = (50,0)
 
-    # label.place(anchor = NW, x=0, y=0)
-
-    #object_pacman["label"] = Label(gui, image = outer_img)
-    #object_pacman["label"].place(anchor = NW, x=0, y=0)
-
-    #objects.append(object_pacman)
-    
-
-initialize_objects()
+object_pacman = dict()
+object_pacman["coordinates"] = (0,0)
+object_pacman["velocity"] = (50,0)
 
 image_path = "resources/pacman.png"
 inner_img = Image.open(image_path)
 inner_img = inner_img.resize((40,40))
 outer_img = ImageTk.PhotoImage(inner_img)
-label = Label(gui, image = outer_img)
-label.place(anchor = NW, x=0, y=0)
+object_pacman["label"] = Label(gui, image = outer_img)
+object_pacman["label"].place(anchor = NW, x=0, y=0)
+objects.append(object_pacman)
 
-#thread_update_and_render = threading.Thread(target=update_and_render)
-# TODO: Make the pacman move
+thread_update_and_render = threading.Thread(target=update_and_render)
+# TODO: Close the started thread
 # TODO: Make the objects object-oriented
 
 # TODO: Add a thread to listen for user input from the keyboard
 
 
 # Start threads
-#thread_update_and_render.start()
+thread_update_and_render.start()
 
 
 gui.mainloop()
--- update_and_render_module.py
+++ update_and_render_module.py
@@ -1,22 +1,15 @@
+from tkinter import *
 from settings import objects
 import time
 
-update_interval = 0.1
-
-def update_and_render():
-    pass
-    # while(True):
-    #     update()
-    #     render()
-    #     time.sleep(update_interval)
-
+update_interval = 0.05
 
 def update():
     for one_object in objects:
         #one_object.update()
         velocity = one_object["velocity"]
         coordinates = one_object["coordinates"]
-        new_coordinates = []
+        new_coordinates = [0,0]
         new_coordinates[0] = coordinates[0] + velocity[0] * update_interval
         new_coordinates[1] = coordinates[1] + velocity[1] * update_interval
         one_object["coordinates"] = new_coordinates
@@ -29,3 +22,9 @@ def render():
         one_object["label"].place(anchor = NW, x=coordinates[0], y=coordinates[1])
     #render_background()
     #render_objects()
+
+def update_and_render():
+    while(True):
+        update()
+        render()
+        time.sleep(update_interval)
