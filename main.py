332d0c30168145808eb8cc684d30085a7eef236b
Caleb Short <calebshort4697@gmail.com>
2020-10-17 19:54:44 -0400

    Keep pacman within the screen

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 from enum import Enum
+import constants
 
 # Initialize pygame
 pygame.init()
 
 # Create the screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
 
 # Title
 pygame.display.set_caption("Pacman")
@@ -18,7 +19,7 @@ class Direction(Enum):
 
 # Player
 pacman_initial_image = pygame.image.load("resources/pacman.png")
-pacman_image = pygame.transform.scale(pacman_initial_image, (40,40))
+pacman_image = pygame.transform.scale(pacman_initial_image, (constants.PACMAN_SIZE,constants.PACMAN_SIZE))
 x = 300
 y = 400
 speed = 0.2
@@ -37,6 +38,14 @@ def calculate_velocity():
         y -= speed
     elif pacman_direction == Direction.DOWN:
         y += speed
+    if x < 0:
+        x = 0
+    if x > constants.SCREEN_WIDTH - constants.PACMAN_SIZE:
+        x = constants.SCREEN_WIDTH - constants.PACMAN_SIZE
+    if y < 0:
+        y = 0
+    if y > constants.SCREEN_HEIGHT - constants.PACMAN_SIZE:
+        y = constants.SCREEN_HEIGHT - constants.PACMAN_SIZE
 
 def handle_keystroke(key):
     global pacman_direction
