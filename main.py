38d8839aeb1819cc8584f57bdae12633d39f2a7e
Caleb Short <calebshort4697@gmail.com>
2020-10-18 13:05:53 -0400

    Made character object and put pacman in there

--- main.py
+++ main.py
@@ -11,89 +11,87 @@ screen = pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT
 # Title
 pygame.display.set_caption("Pacman")
 
-# Player
-pacman_initial_image = pygame.image.load("resources/pacman.png")
-pacman_image = pygame.transform.scale(pacman_initial_image, (constants.CHARACTER_SIZE,constants.CHARACTER_SIZE))
-x = 300
-y = 400
-speed = 0.2
-pacman_direction = constants.Direction.RIGHT
+# Pacman
+pacman = objects.Character(screen, 300, 400, constants.Direction.RIGHT, "resources/pacman.png")
+# pacman_initial_image = pygame.image.load("resources/pacman.png")
+# pacman_image = pygame.transform.scale(pacman_initial_image, (constants.CHARACTER_SIZE,constants.CHARACTER_SIZE))
+# x = 300
+# y = 400
+# speed = 0.2
+# pacman_direction = constants.Direction.RIGHT
 
+# Walls
 wall = objects.Wall(screen, 500, 300, constants.Orientation.VERTICAL, 200)
 
-def player(x, y):
-    screen.blit(pacman_image, (x,y))
+# def overlapping_horizontal(wall):
+#     pacman_left = x
+#     pacman_right = x + constants.CHARACTER_SIZE
+#     # Pacman's edge is within wall's frame
+#     if (pacman_left < wall.right and pacman_left > wall.left) or (pacman_right < wall.right and pacman_right > wall.left):
+#         return True
+#     # Wall's edge is withing pacman's frame
+#     if (wall.left < pacman_right and wall.left > pacman_left) or (wall.right < pacman_right and wall.right > pacman_left):
+#         return True
+#     return False
 
+# def overlapping_vertical(wall):
+#     pacman_top = y
+#     pacman_bottom = y + constants.CHARACTER_SIZE
+#     # Pacman's edge is within wall's frame
+#     if (pacman_top < wall.bottom and pacman_top > wall.top) or (pacman_bottom < wall.bottom and pacman_bottom > wall.top):
+#         return True
+#     # Wall's edge is withing pacman's frame
+#     if (wall.top < pacman_bottom and wall.top > pacman_top) or (wall.bottom < pacman_bottom and wall.bottom > pacman_top):
+#         return True
+#     return False
 
-def overlapping_horizontal(wall):
-    pacman_left = x
-    pacman_right = x + constants.CHARACTER_SIZE
-    # Pacman's edge is within wall's frame
-    if (pacman_left < wall.right and pacman_left > wall.left) or (pacman_right < wall.right and pacman_right > wall.left):
-        return True
-    # Wall's edge is withing pacman's frame
-    if (wall.left < pacman_right and wall.left > pacman_left) or (wall.right < pacman_right and wall.right > pacman_left):
-        return True
-    return False
+# def overlapping(wall):
+#     return overlapping_horizontal(wall) and overlapping_vertical(wall)
 
-def overlapping_vertical(wall):
-    pacman_top = y
-    pacman_bottom = y + constants.CHARACTER_SIZE
-    # Pacman's edge is within wall's frame
-    if (pacman_top < wall.bottom and pacman_top > wall.top) or (pacman_bottom < wall.bottom and pacman_bottom > wall.top):
-        return True
-    # Wall's edge is withing pacman's frame
-    if (wall.top < pacman_bottom and wall.top > pacman_top) or (wall.bottom < pacman_bottom and wall.bottom > pacman_top):
-        return True
-    return False
-
-def overlapping(wall):
-    return overlapping_horizontal(wall) and overlapping_vertical(wall)
-
-def calculate_velocity():
-    global x, y, speed, wall
-    old_x = x
-    old_y = y
-    if pacman_direction == constants.Direction.RIGHT:
-        x += speed
-    elif pacman_direction == constants.Direction.LEFT:
-        x -= speed
-    elif pacman_direction == constants.Direction.UP:
-        y -= speed
-    elif pacman_direction == constants.Direction.DOWN:
-        y += speed
-    # Handle the case where we've reached the edge of the screen
-    if x < 0:
-        x = 0
-    if x > constants.SCREEN_WIDTH - constants.CHARACTER_SIZE:
-        x = constants.SCREEN_WIDTH - constants.CHARACTER_SIZE
-    if y < 0:
-        y = 0
-    if y > constants.SCREEN_HEIGHT - constants.CHARACTER_SIZE:
-        y = constants.SCREEN_HEIGHT - constants.CHARACTER_SIZE
-    # Handle the case where we've run into a wall
-    if overlapping(wall):
-        x = old_x
-        y = old_y
+# def move():
+#     global x, y, speed, wall
+#     old_x = x
+#     old_y = y
+#     if pacman_direction == constants.Direction.RIGHT:
+#         x += speed
+#     elif pacman_direction == constants.Direction.LEFT:
+#         x -= speed
+#     elif pacman_direction == constants.Direction.UP:
+#         y -= speed
+#     elif pacman_direction == constants.Direction.DOWN:
+#         y += speed
+#     # Handle the case where we've reached the edge of the screen
+#     if x < 0:
+#         x = 0
+#     if x > constants.SCREEN_WIDTH - constants.CHARACTER_SIZE:
+#         x = constants.SCREEN_WIDTH - constants.CHARACTER_SIZE
+#     if y < 0:
+#         y = 0
+#     if y > constants.SCREEN_HEIGHT - constants.CHARACTER_SIZE:
+#         y = constants.SCREEN_HEIGHT - constants.CHARACTER_SIZE
+#     # Handle the case where we've run into a wall
+#     if overlapping(wall):
+#         x = old_x
+#         y = old_y
 
 
 def handle_keystroke(key):
-    global pacman_direction
+    global pacman
     if key == pygame.K_LEFT:
-        pacman_direction = constants.Direction.LEFT
+        pacman.direction = constants.Direction.LEFT
     elif key == pygame.K_RIGHT:
-        pacman_direction = constants.Direction.RIGHT
+        pacman.direction = constants.Direction.RIGHT
     elif key == pygame.K_UP:
-        pacman_direction = constants.Direction.UP
+        pacman.direction = constants.Direction.UP
     elif key == pygame.K_DOWN:
-        pacman_direction = constants.Direction.DOWN
+        pacman.direction = constants.Direction.DOWN
 
 
 # Game loop
 running = True
 while running:
     screen.fill(constants.BLACK)
-    calculate_velocity()
+    pacman.move(wall)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -102,6 +100,6 @@ while running:
 
 
 
-    player(x, y)
+    pacman.render()
     wall.render()
     pygame.display.update()
--- objects.py
+++ objects.py
@@ -2,12 +2,43 @@ import pygame
 import constants
 
 class Object:
-    def __init__(self, screen, left, right, top, bottom):
+    def __init__(self, screen, x, width, y, height):
         self.screen = screen
-        self.left = left
-        self.right = right
-        self.top = top
-        self.bottom = bottom
+        self.x = x
+        self.width = width
+        self.y = y
+        self.height = height
+
+    def overlapping_horizontal(self, other):
+        self_left = self.x
+        self_right = self.x + self.width
+        other_left = other.x
+        other_right = other.x + other.width
+
+        # This's edge is within other's frame
+        if (self_left < other_right and self_left > other_left) or (self_right < other_right and self_right > other_left):
+            return True
+        # Other's edge is within this's frame
+        if (other_left < self_right and other_left > self_left) or (other_right < self_right and other_right > self_left):
+            return True
+        return False
+
+    def overlapping_vertical(self, other):
+        self_top = self.y
+        self_bottom = self.y + self.height
+        other_top = other.y
+        other_bottom = other.y + other.height
+
+        # This's edge is within other's frame
+        if (self_top < other_bottom and self_top > other_top) or (self_bottom < other_bottom and self_bottom > other_top):
+            return True
+        # Other's edge is within this's frame
+        if (other_top < self_bottom and other_top > self_top) or (other_bottom < self_bottom and other_bottom > self_top):
+            return True
+        return False
+
+    def overlapping(self, other):
+        return self.overlapping_horizontal(other) and self.overlapping_vertical(other)
 
 class Wall(Object):
     def __init__(self, screen, left, top, orientation, length):
@@ -21,8 +52,43 @@ class Wall(Object):
             height = length
         else:
             raise TypeError("The orientation provided is invalid")
-        super(Wall, self).__init__(screen, left, left + width, top, top + length)
+        super(Wall, self).__init__(screen, left, width, top, height)
         self.object = pygame.Rect(left, top, width, height)
 
     def render(self):
         pygame.draw.rect(self.screen, constants.BLUE, self.object)
+
+class Character(Object):
+    def __init__(self, screen, x, y, direction, image_path):
+        super(Character, self).__init__(screen, x, constants.CHARACTER_SIZE, y, constants.CHARACTER_SIZE)
+        initial_image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(initial_image, (constants.CHARACTER_SIZE,constants.CHARACTER_SIZE))
+        self.direction = direction
+
+    def render(self):
+        self.screen.blit(self.image, (self.x, self.y))
+    
+    def move(self, wall):
+        old_x = self.x
+        old_y = self.y
+        if self.direction == constants.Direction.RIGHT:
+            self.x += constants.CHARACTER_SPEED
+        elif self.direction == constants.Direction.LEFT:
+            self.x -= constants.CHARACTER_SPEED
+        elif self.direction == constants.Direction.UP:
+            self.y -= constants.CHARACTER_SPEED
+        elif self.direction == constants.Direction.DOWN:
+            self.y += constants.CHARACTER_SPEED
+        # Handle the case where we've reached the edge of the screen
+        if self.x < 0:
+            self.x = 0
+        if self.x > constants.SCREEN_WIDTH - constants.CHARACTER_SIZE:
+            self.x = constants.SCREEN_WIDTH - constants.CHARACTER_SIZE
+        if self.y < 0:
+            self.y = 0
+        if self.y > constants.SCREEN_HEIGHT - constants.CHARACTER_SIZE:
+            self.y = constants.SCREEN_HEIGHT - constants.CHARACTER_SIZE
+        # Handle the case where we've run into a wall
+        if self.overlapping(wall):
+            self.x = old_x
+            self.y = old_y
